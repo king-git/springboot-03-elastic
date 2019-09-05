94c15511c872f594eca47901d0645d686dbf24f4
caihao <kingch92@163.com>
2019-09-05 21:55:02 +0800

    Initial commit

---
+++ src/main/java/com/caihao/elastic/Springboot03ElasticApplication.java
@@ -0,0 +1,19 @@
+package com.caihao.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ *
+ */
+
+@SpringBootApplication
+public class Springboot03ElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03ElasticApplication.class, args);
+	}
+
+}
